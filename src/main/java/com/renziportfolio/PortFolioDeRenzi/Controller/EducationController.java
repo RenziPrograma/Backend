a446e1537dd1f3dcd43d8a5b6d62563c4b3dec08
Renzi <renziprograma@gmail.com>
2022-12-26 21:11:54 -0300

    Con los links ajustados para el deploy

--- src/main/java/com/renziportfolio/PortFolioDeRenzi/Controller/EducationController.java
+++ src/main/java/com/renziportfolio/PortFolioDeRenzi/Controller/EducationController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/education")
-@CrossOrigin(origins = "Http://localhost:4200")
+@CrossOrigin(origins = "https://frontendrenziprograma.web.app")
 public class EducationController {
 
     @Autowired
--- src/main/java/com/renziportfolio/PortFolioDeRenzi/Controller/WorkExperienceController.java
+++ src/main/java/com/renziportfolio/PortFolioDeRenzi/Controller/WorkExperienceController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "Http://localhost:4200")
+@CrossOrigin(origins = "https://frontendrenziprograma.web.app")
 public class WorkExperienceController {
 
     @Autowired
